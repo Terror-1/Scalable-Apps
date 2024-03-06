697e045b7f3c491b4b6c4fe25d8f02e54f2b4a51
kareemoo <kareem.m.eid@gmail.com>
2024-03-06 21:23:40 +0200

    maven setup

---
+++ wall-app/src/main/java/com/wall/app/App.java
@@ -0,0 +1,13 @@
+package com.wall.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
