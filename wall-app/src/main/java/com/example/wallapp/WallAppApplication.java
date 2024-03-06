f12ef4127b7c5c29941afceb4d7027a39c5cb49a
AhmedHatem0 <ahmedhateem315@gmail.com>
2024-03-06 23:17:24 +0200

    spring project initialized

---
+++ wall-app/src/main/java/com/example/wallapp/WallAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallAppApplication.class, args);
+    }
+
+}
---
+++ wall-app/src/test/java/com/example/wallapp/WallAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
