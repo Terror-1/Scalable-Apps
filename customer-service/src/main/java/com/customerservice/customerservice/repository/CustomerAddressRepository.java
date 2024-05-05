a6ddcfe05b9822f1f5bcb62b1a0589b88b9122e7
Ahmad Bodayr <ahmadbodayr@Ahmads-MacBook-Pro.local>
2024-05-05 07:05:35 +0300

    fixed a bug

--- customer-service/src/main/java/com/customerservice/customerservice/repository/CustomerAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.customerservice.customerservice.repository;
-
-import com.customerservice.customerservice.entity.CustomerAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {
-}
--- customer-service/src/main/java/com/customerservice/customerservice/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.customerservice.customerservice.repository;
-
-import com.customerservice.customerservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
