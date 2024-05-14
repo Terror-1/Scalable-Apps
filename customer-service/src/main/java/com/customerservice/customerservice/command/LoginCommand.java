1916935bf49787b9441f7106b20ac541732e5833
kareemoo <kareem.m.eid@gmail.com>
2024-05-14 22:45:16 +0300

    command pattern for customer service

---
+++ customer-service/src/main/java/com/customerservice/customerservice/command/LoginCommand.java
@@ -0,0 +1,44 @@
+package com.customerservice.customerservice.command;
+
+import com.customerservice.customerservice.entity.MyCustomer;
+import com.customerservice.customerservice.service.CustomerService;
+import com.stripe.exception.StripeException;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.HttpHeaders;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Duration;
+
+@AllArgsConstructor
+public class LoginCommand implements Command<ResponseEntity<String>> {
+
+    private final HttpServletResponse response;
+
+    private final MyCustomer customer;
+
+    private final CustomerService customerService;
+
+    @Override
+    public ResponseEntity<String> execute() throws StripeException {
+        String my_token = customerService.authenticate(customer.getEmail(), customer.getPassword());
+        if (my_token != null) {
+            // Create a cookie with the token
+            ResponseCookie cookie = ResponseCookie.from("token", my_token)
+                    .maxAge(Duration.ofDays(1)) // Set the cookie's maximum age
+                    .path("/") // Set the cookie's path
+                    .httpOnly(true) // Set the cookie as HttpOnly to prevent client-side script access
+                    .build();
+
+            // Add the cookie to the response header
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+            customerService.createSession(customer.getEmail());
+            return new ResponseEntity<>(my_token, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
